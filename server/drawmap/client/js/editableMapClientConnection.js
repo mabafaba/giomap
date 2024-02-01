bf5e243dd603f28a4baf41a173257a42da0f5554
mabafaba <m@martinbarner.de>
2024-02-01 14:05:20 +0100

    rm clear all button

--- server/drawmap/client/js/editableMapClientConnection.js
+++ server/drawmap/client/js/editableMapClientConnection.js
@@ -24,7 +24,11 @@ mapio = function(map, mapCanvasShareLinkId, onEachNewFeature, editingLayer){
         },
         
         addEditingControls: function (map) {
-            
+                        // exclude clear all button from editing controls
+            L.EditToolbar.Delete.include({
+                removeAllLayers: false
+            });
+
             // Initialise the draw control and pass it the FeatureGroup of editable layers
             var drawControl = new L.Control.Draw({
                 edit: {
